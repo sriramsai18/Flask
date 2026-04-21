9f56512c07fc4f8c1654afc14788cc1ddf9b92c4
SRIRAM SAI <119840345+sriramsai18@users.noreply.github.com>
2026-04-21 07:37:34 +0530

    Create index.py

---
+++ api/index.py
@@ -0,0 +1,10 @@
+from flask import Flask, redirect
+
+app = Flask(__name__)
+
+# This sends anyone who hits the Vercel URL to your Render URL
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    # Change the URL below if you renamed your Render app!
+    return redirect("https://onrender.com", code=301)
